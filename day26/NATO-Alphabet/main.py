7e2a3d5b3591c82a1ff5f28334f08b51c981dbac
Amit kumar ahirwar <70595773+roronoazoro1807@users.noreply.github.com>
2025-03-01 22:58:07 +0530

    Update main.py

--- day26/NATO-Alphabet/main.py
+++ day26/NATO-Alphabet/main.py
@@ -3,7 +3,7 @@ import pandas
 # Load the NATO phonetic alphabet data
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-# Create a dictionary {Letter: Code}
+# Create a dictionary
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 # Get user input and convert it to phonetic code
